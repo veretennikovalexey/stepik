587ffaf01be7f6253564d481f32b0f5dd4c08c29
veretennikovalexey <raidex@yandex.ru>
2024-10-05 12:58:58 +0300

    ss

---
+++ 3.5/main.js
@@ -0,0 +1,5 @@
+const str = "    23rub    ";
+
+document.getElementById("result1").textContent = Number.parseInt(str);
+document.getElementById("result2").textContent = parseInt(str);
+document.getElementById("result3").textContent = +str;
