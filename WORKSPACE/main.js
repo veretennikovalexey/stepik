2fc891146bdd282977ae408e7c27c0b7d44ab28d
veretennikovalexey <raidex@yandex.ru>
2024-10-06 09:43:42 +0300

    price * quantity

--- WORKSPACE/main.js
+++ WORKSPACE/main.js
@@ -1,17 +1,22 @@
 "use strict";
+const price = 6.4;
+const quantity = 3;
+const sum = price * quantity;
+console.log(sum);
+
 // console.log(2 ** 53 - 1);
 // console.log(Number.MAX_SAFE_INTEGER);
 // console.log(Number.MAX_VALUE);
 // console.log(0.1 + 0.2);
 
-let num = 24.3456;
-console.log(num); // 24.3456
-num = num.toFixed(5); // 24.34560
-console.log(num);
-console.log(10 + num); // 1024.34560
-num = 10 + +num;
-console.log(num); // 34.345600000000005
-num = num * 100;
-num = Math.round(num, 2);
-num = num / 100;
-console.log(num); // 34.35
+// let num = 24.3456;
+// console.log(num); // 24.3456
+// num = num.toFixed(5); // 24.34560
+// console.log(num);
+// console.log(10 + num); // 1024.34560
+// num = 10 + +num;
+// console.log(num); // 34.345600000000005
+// num = num * 100;
+// num = Math.round(num, 2);
+// num = num / 100;
+// console.log(num); // 34.35
